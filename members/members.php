21be52192e699b89971f065dd2fee1264bfe41ee
liartfr <viriisxp@gmail.com>
2018-11-24 19:47:09 +0100

    Create members.php

---
+++ members/members.php
@@ -0,0 +1,25 @@
+<?php
+
+// You have to add :
+//		elseif ($WHERE_AM_I == 'members') {
+//			include(THEME_DIR_PHP.'membres.php');
+//	}
+// into your index.php to have an access to this one.
+
+?>
+
+<?php
+echo '<section>'.PHP_EOL;
+	 if (pluginActivated('pluginMembers')){
+				if (!empty($s_member)){
+					
+					echo '<h1>'.$s_member.'</h1>'.PHP_EOL;
+
+				}
+				else{
+					echo '<h1>'.$L->get('all_members').'</h1>'.PHP_EOL;
+				}
+		}
+		else{echo '<h1>Acces forbidden or plugin Unactiv</h1>';}
+echo '</section>'.PHP_EOL;
+?>
