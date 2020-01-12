b90df173b9c075449c684285e9094b6a41322452
liartfr <viriisxp@gmail.com>
2020-01-12 04:25:20 +0100

    Update members.php

--- members/members.php
+++ members/members.php
@@ -1,89 +1,107 @@
 <?php
-echo '<section id="membres">'.PHP_EOL;
-if (pluginActivated('pluginMembers')){
-    if ($s_member == true){
-        if ($s_member_exist == true){ // we found the user and he accept to display his information :
-            $user = new User($selected_member);
-            ?>
-            <div class="main">
-                <div class="bio">
-                    <img class="profile-img" src="<?php if ($user->profilePicture()){echo $user->profilePicture();}else{echo 'https://picsum.photos/200';} ?>"/>
-                    <h3 class="header"><?php if ($user->nickname()){echo $user->nickname();} else {echo $selected_member;} ?></h3>
-                    <p><?php echo $user->lastName()?> <?php echo $user->firstName() ?></p>
-                </div>
-                <div class="contact">
-                    <?php
-                    echo '<ul class="user_social">';
-                    foreach(Theme::socialNetworks() as $social => $label ) {
-                        if ( $user->{$social}() ) {
-                            echo '<li><i class="fab fa-'.$social.'"></i>  <a href="'.$user->$social().'" >'.$label.'</a></li>';
-                        }
-                    }
-                    echo '</ul>';
-                    ?>
-                </div>
-            </div>
-
-
-            <?php
-        }
-        else{
-            echo '<h1>'.$L->get('members').'</h1>';
-            echo '<h3>'.$L->get('hidden_member').'</h3>'.PHP_EOL;
-        }
-    }
-    else{
-      ?>
-      <header id="fh5co-header" role="banner" class="fh5co-project js-fh5co-waypoint no-border" data-colorbg="#222222" data-next="yes">
-        <div class="container">
-          <div class="fh5co-text-wrap animate-box">
-            <div class="fh5co-intro-text">
-              <h1><?php echo  '<h1>'.$L->get('all_members').'</h1>'.PHP_EOL;?> </h1>
-            </div>
-          </div>
-        </div>
-        <div class="btn-next animate-box fh5co-learn-more">
-          <a href="#" class="scroll-btn">
-            <span>Voir la liste</span>
-            <i class="icon-chevron-down"></i>
-          </a>
-        </div>
-      </header>
-
-      <div class="js-fh5co-waypoint fh5co-project-detail" id="fh5co-main" data-colorbg="">
-        <div class="container">
-        <div class="row row-bottom-padded-sm animate-box" id="2009/2019">
-          <div class="col-md-3">
-            <h3 class="fh5co-section-heading"><span class="fh5co-number">[B2P]</span>Les Membres</h3>
-          </div>
-          <div class="col-md-9">
-            <ul class="fh5co-list-style-1 fh5co-inline">
-            <?php
-
-
-            global $users;
-            $list = $users->keys();
-            foreach ($list as $username) {
-              $B2P = new User($username);
-              ?>
-                <li><a href="<?php echo $url->slug().'/'.$B2P->nickname(); ?>"><?php echo $B2P->nickname(); ?></li>
-
-              <?php
-            }
-             ?>
-           </ul>
-         </div>
-       </div>
-     </div>
-   </div>
-
-
-        <?php
-    }
-}
-else
+class pluginMembers extends Plugin {
+private $s_member=null;
+private $s_member_exist=null;
+private $selected_member=null;
+public function init()
 {
-    echo '<h1>Acces forbidden or plugin Unactiv</h1>';
+	// We collect the list of members
+	// We put all value in a array for visibility of the user.
+	// For Privacy reason (like RGPD), All value start with "false" attribut.
+	global $users;
+	$list = $users->keys();
+	foreach ($list as $username) {
+		try {
+		$user = new User($username);
+		if (empty($this->dbFields)){
+			$this->dbFields = array($username => false);
+		} else {
+			$this->dbFields = array_merge($this->dbFields, array($username => false));
+		}
+		} catch (Exception $e) {
+		// Continue
+		}
+	} // End of foreach
+}  //end of INIT
+public function beforeAll()
+{
+	// Check if the URL match with the webhook
+	$webhook = 'membres';
+	if ($this->webhook($webhook, false, false)) {
+	global $site;
+	global $url;
+	global $s_member;
+	$url->setWhereAmI($webhook); // THE CORRECT WEBHOOK !!!
+	// Get username from URI
+	$this->username = $this->webhook($webhook, true, false);
+	$this->username = trim($this->username, '/');
+	if (empty($this->username)) {
+		$s_member = false;
+	//var_dump('Without username defined');
+	} else {
+		$s_member = $this->username;
+	//var_dump('Username defined: '.$this->username);
+	}
+	}
 }
-echo '</section>'.PHP_EOL;
-?>
+public function siteBodyBegin()
+{
+	$webhook = 'members';
+	if ($this->webhook($webhook, false, false)) {
+		global $url;
+		global $WHERE_AM_I;
+		global $s_member;
+		global $s_member_exist;
+		global $selected_member;
+		$url->setWhereAmI($webhook);
+		// Get the pre-defined variable from the rule 69.pages.php
+		// We change the content to show in the website
+		if (empty($this->username)) {
+			$s_member = false;
+			//exit('Without username defined');
+		} else {
+			$s_member = true;
+			$selected_member = $this->username;
+			if ($this->getValue($selected_member)===true){
+			$s_member_exist = true;
+			//var_dump('Username defined: '.$this->username);
+			}
+		}
+	} //endif webhook
+} // End siteBodyBegin
+public function form()
+{
+	global $L;
+	global $users;
+	$list = $users->keys();
+	// Light customized form
+	$html = '<style>.col-md-4{text-transform:uppercase;}.col-md-4 label{border-bottom:1px dotted grey;width:200px;}.col-md-4 select{height:30px;width:200px;text-transform: uppercase;}';
+	$html .= '.col-md-4 label{ }';
+	$html .= '</style>'.PHP_EOL;
+	$html  .= '<div class="alert alert-primary" role="alert">'.PHP_EOL;
+	$html .= $this->description();
+	$html .= '</div>'.PHP_EOL;
+	// Who do you want to see ?
+	$html .= '<div style="border:1px solid #ccc;padding:10px;border-radius:5px;">'.PHP_EOL;
+	$html .= '<h5 style="text-decoration:underline;">'.$L->get('who_do_you_want_to_see').' </h5>'.PHP_EOL;
+	$html .= '<div class="row">';
+	$list = $users->keys();
+	foreach ($list as $username) {
+		try {
+			$user = new User($username);
+			$html .= '<div class="col-md-4">'.PHP_EOL;
+			$html .= '<label>'.$username.'</label>'.PHP_EOL;
+			$html .= '<select name="'.$username.'">'.PHP_EOL;
+			$html .= '<option value="1" '.($this->getValue($username)===true?'selected':'').'>'.$L->get('display').'</option>'.PHP_EOL;
+			$html .= '<option value="0" '.($this->getValue($username)===false?'selected':'').'>'.$L->get('hide').'</option>'.PHP_EOL;
+			$html .= '</select>'.PHP_EOL;
+			$html .= '</div>'.PHP_EOL;
+		} catch (Exception $e) {
+		// Continue
+		}
+	} //foreach username
+	$html .= '</div>';
+	$html .= '</div>'.PHP_EOL;
+	return $html;
+} // End form
+} //end plugin
